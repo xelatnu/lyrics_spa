c50b56eaa9d5e9ed8b10cf22f285c357cefdf1ea
Alexei Untila <alexei.untila@stefanini.com>
2022-11-14 17:39:27 +0200

    feat: Custom Hook

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,33 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import LyricsHeader from '../components/LyricsHeader';
 import LyricsTable from '../components/LyricsTable';
-import lyricsService from '../features/lyricsService';
+// import lyricsService from '../features/lyricsService';
 import LoadingSpinner from '../components/loadingSpinner/loadingSpinner';
+import useLyricsSearch from '../components/hooks/useLyricsSearch';
 import './app.css';
 
-function App() {
-  const [lyricsData, setLyricsData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const searchLyrics = async ({ lyrics, artist }) => {
-    try {
-      setIsLoading(true);
-      const res = await lyricsService.getLyrics(lyrics, artist);
-      setLyricsData(res);
-      setIsLoading(false);
-    } catch (error) {
-      setIsLoading(false);
-      alert('Please wait a few minutes before try again');
-    }
-  };
+const App = () => {
+  const [searchLyrics, { isLoading, lyricsData }] = useLyricsSearch();
 
   return (
     <div className="app">
-      {isLoading ? <LoadingSpinner />
+      {isLoading
+        ? <LoadingSpinner />
         : <LyricsHeader searchLyrics={searchLyrics} />}
-      {lyricsData.length ? <LyricsTable lyricsData={lyricsData} /> : null}
+      {lyricsData?.length ? <LyricsTable lyricsData={lyricsData} /> : null}
     </div>
   );
-}
+};
 
 export default App;
