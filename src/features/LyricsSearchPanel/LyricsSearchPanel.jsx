616dcdcd461095b347753e6e92e53f8d3d3eee04
Alexei Untila <alexei.untila@stefanini.com>
2022-11-30 10:21:27 +0200

    feat:formValidation

--- src/features/LyricsSearchPanel/LyricsSearchPanel.jsx
+++ src/features/LyricsSearchPanel/LyricsSearchPanel.jsx
@@ -1,25 +1,22 @@
+/* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/function-component-definition */
-import React, { useState } from 'react';
 import './lyrics-search-panel.css';
-
-const defValues = {
-  lyrics: '',
-  artist: '',
-};
+import { useForm } from 'react-hook-form';
 
 // eslint-disable-next-line react/prop-types
 function LyricsSearchPanel({ onSearch }) {
-  const [formData, setFormData] = useState(defValues);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isValid },
+  } = useForm({
+    mode: 'onBlur',
+  });
 
-  const onChange = (event) => {
-    event.preventDefault();
-    setFormData((prevData) => ({ ...prevData, [event.target.name]: event.target.value }));
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    onSearch(formData);
-    setFormData(defValues);
+  const onSubmit = (data) => {
+    onSearch(data);
+    reset();
   };
 
   return (
@@ -32,27 +29,32 @@ function LyricsSearchPanel({ onSearch }) {
       >
         Search your favorite song lyrics
       </h2>
-      <form onSubmit={handleSubmit} className="form">
+      <form onSubmit={handleSubmit(onSubmit)} className="form">
         <input
-          data-testid="lyrics-input"
-          name="lyrics"
-          value={formData.lyrics}
-          onChange={onChange}
-          type="text"
-          className="lyrics-input "
-          placeholder="Type lyrics"
-          required
+          className="search-input"
+          {...register('lyrics', {
+            required: 'Lyrics input is required',
+            maxLength: 35,
+            minLength: {
+              value: 2,
+              message: 'Type min one character',
+            },
+          })
+          }
         />
+        {errors?.lyrics && <h6>{errors?.lyrics.message || 'Error'}</h6>}
         <input
-          data-testid="artist-input"
-          name="artist"
-          value={formData.artist}
-          onChange={onChange}
-          type="text"
-          className="search-input "
-          placeholder="Type artist"
+          className="search-input"
+          {...register('artist', {
+            maxLength: {
+              value: 35,
+              message: 'Artist is to long',
+            },
+          })
+            }
         />
-        <button data-testid="submit-button" type="submit" className="btn btn-primary">Search lyrics</button>
+        {errors?.artist && <h6>{errors?.artist.message || 'Error'}</h6>}
+        <button data-testid="submit-button" type="submit" className="btn btn-primary" disabled={!isValid}>Search lyrics</button>
       </form>
     </div>
   );
